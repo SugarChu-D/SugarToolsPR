4b9ee326a43be439b0f1f2e1e0c86db420a34546
Sugar <gogo7260koh@hotmail.com>
2026-02-07 12:23:17 +0900

    grotto多分完成

--- rng-core/src/lcg/grotto.rs
+++ rng-core/src/lcg/grotto.rs
@@ -1,8 +1,105 @@
 use super::Lcg;
 
-#[derive(Debug, Clone)]
-pub struct grotto {
-    is_filled: bool,
-    sub_slot: u8,
-    slot: u8,
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Grotto {
+    sub_slot: Option<u32>,
+    slot: Option<u32>,
+    gender: Option<u32>
+}
+
+pub struct Grottos {
+    grottos: [Grotto; 20]
+}
+
+impl Grottos {
+    pub fn new () -> Self {
+        Self { grottos: [Grotto::default(); 20] }
+    }
+
+    pub fn new_game () -> Self {
+        Self { grottos: Self::new_game_grottos() }
+    }
+
+    pub fn reset_newgame(&mut self) {
+        self.grottos = Self::new_game_grottos();
+    }
+
+    pub fn fill_grottos(&mut self, lcg: &Lcg) {
+        let mut lcg_local = lcg.clone();
+        for i in 0..20 {
+            // もし埋まっているならスキップ
+            if self.grottos[i].sub_slot.is_some() {continue;}
+            // lcgが5以上なら埋めないでスキップ
+            if lcg_local.rand(100) >= 5 {continue;}
+
+            self.grottos[i].sub_slot = Some(lcg_local.rand(4));
+            self.grottos[i].slot = Some(lcg_local.rand(100));
+            self.grottos[i].gender = Some(lcg_local.rand(100));
+        }
+    }
+
+    fn new_game_grottos() -> [Grotto; 20] {
+        [
+            Grotto::default(),
+            Grotto{
+                sub_slot: Some(1),
+                slot: Some(0),
+                gender: Some(0)
+                // 5番道路のチラーミィはデフォルトで埋まってる
+            },
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+            Grotto::default(),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_candy() {
+        let mut seed = Lcg::new(0xf9d9dd91248eecb0);
+        seed.advance(385);
+        let mut grottos = Grottos::new_game();
+        grottos.fill_grottos(&seed);
+
+        for i in 0..20 {
+            println!(
+                "{}: sub_slot={:?}, slot={:?}",
+                i, grottos.grottos[i].sub_slot, grottos.grottos[i].slot
+            );
+        }
+    }
+
+    #[test]
+    fn test_get_candy_dragonite() {
+        let mut seed = Lcg::new(0x113E10468C85C156);
+        seed.advance(395);
+        let mut grottos = Grottos::new();
+        grottos.fill_grottos(&seed);
+
+        for i in 0..20 {
+            println!(
+                "{}: sub_slot={:?}, slot={:?}, gender={:?}",
+                i, grottos.grottos[i].sub_slot, grottos.grottos[i].slot, grottos.grottos[i].gender
+            );
+        }
+    }
 }
