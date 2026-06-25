f653bfa6302c54b7c0916075ffec027a3f5e8970
Sugar <gogo7260koh@hotmail.com>
2026-06-26 00:41:02 +0900

    FIX: 砂煙を軽量化

--- rng-core/src/lcg/cloud_impl.rs
+++ rng-core/src/lcg/cloud_impl.rs
@@ -1,6 +1,7 @@
 use super::Lcg;
 use std::cmp::max;
 use std::cmp::min;
+use std::collections::HashSet;
 
 static RANGES: [[i32; 4]; 4] = [
     [0, 5, -5, 5],
@@ -9,38 +10,90 @@ static RANGES: [[i32; 4]; 4] = [
     [-5, 5, 0, 5],
 ];
 
+pub trait CloudTileSource {
+    fn contains_tile(&self, tile: &[i32; 2]) -> bool;
+}
+
+impl CloudTileSource for [[i32; 2]] {
+    fn contains_tile(&self, tile: &[i32; 2]) -> bool {
+        self.contains(tile)
+    }
+}
+
+pub struct CloudTileLookup {
+    tiles: HashSet<[i32; 2]>,
+}
+
+impl CloudTileLookup {
+    pub fn new(map_tile: &[[i32; 2]]) -> Self {
+        Self {
+            tiles: map_tile.iter().copied().collect(),
+        }
+    }
+}
+
+impl CloudTileSource for CloudTileLookup {
+    fn contains_tile(&self, tile: &[i32; 2]) -> bool {
+        self.tiles.contains(tile)
+    }
+}
+
 impl Lcg {
     pub fn can_get_cloud(&mut self) -> bool {
         self.next() < 0x1999_9999_9999_9999
     }
 
-    pub fn get_cloud(&mut self, trainer_tile: &[i32; 2], map_tile: &[[i32; 2]]) -> [i32; 2]  {
+    pub fn get_cloud<T: CloudTileSource + ?Sized>(
+        &mut self,
+        trainer_tile: &[i32; 2],
+        map_tile: &T,
+    ) -> [i32; 2] {
         if self.can_get_cloud() == false {
             return [0, 0];
         }
-        
+
         let range = RANGES[(self.rand(4)) as usize];
 
-        let x_min_in_chunk: i32 = max((trainer_tile[0] % 32) as i32 + range[0], 0);
-        let x_max_in_chunk: i32 = min((trainer_tile[0] % 32) as i32 + range[1], 31);
-        let y_min_in_chunk: i32 = max((trainer_tile[1] % 32) as i32 + range[2], 0);
-        let y_max_in_chunk: i32 = min((trainer_tile[1] % 32) as i32 + range[3], 31);
+        let trainer_chunk_x = trainer_tile[0] - (trainer_tile[0] % 32);
+        let trainer_chunk_y = trainer_tile[1] - (trainer_tile[1] % 32);
+        let trainer_offset_x = trainer_tile[0] % 32;
+        let trainer_offset_y = trainer_tile[1] % 32;
 
-        let x_min = x_min_in_chunk + trainer_tile[0] - (trainer_tile[0] % 32);
-        let x_max = x_max_in_chunk + trainer_tile[0] - (trainer_tile[0] % 32);
-        let y_min = y_min_in_chunk + trainer_tile[1] - (trainer_tile[1] % 32);
-        let y_max = y_max_in_chunk + trainer_tile[1] - (trainer_tile[1] % 32);
+        let x_min = max(trainer_offset_x + range[0], 0) + trainer_chunk_x;
+        let x_max = min(trainer_offset_x + range[1], 31) + trainer_chunk_x;
+        let y_min = max(trainer_offset_y + range[2], 0) + trainer_chunk_y;
+        let y_max = min(trainer_offset_y + range[3], 31) + trainer_chunk_y;
 
-        let mut valid_tiles: Vec<[i32; 2]> = Vec::new();
-        for tile_y in y_min..=y_max  {
-            for tile_x in x_min..=x_max  {
+        let mut valid_tile_count = 0u32;
+
+        for tile_y in y_min..=y_max {
+            for tile_x in x_min..=x_max {
                 let tile = [tile_x, tile_y];
-                if map_tile.contains(&tile) && &tile != trainer_tile {
-                    valid_tiles.push(tile);
+                if tile != *trainer_tile && map_tile.contains_tile(&tile) {
+                    valid_tile_count += 1;
                 }
             }
         }
 
-        return valid_tiles[(self.rand(valid_tiles.len() as u64)) as usize];
+        if valid_tile_count == 0 {
+            return [0, 0];
+        }
+
+        let target_index = self.rand(valid_tile_count as u64);
+        let mut current_index = 0u32;
+
+        for tile_y in y_min..=y_max {
+            for tile_x in x_min..=x_max {
+                let tile = [tile_x, tile_y];
+                if tile != *trainer_tile && map_tile.contains_tile(&tile) {
+                    if current_index == target_index {
+                        return tile;
+                    }
+                    current_index += 1;
+                }
+            }
+        }
+
+        return [0, 0];
     }
 }
